0ab3317f5ba96356ad37fea2e3bdb994d0ccd96c
Syed Mustafa <39621028+SMMustafa@users.noreply.github.com>
2021-05-04 16:59:24 -0400

    Add files via upload

---
+++ LC287_FindTheDuplicateNumber.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+
+        count = Counter(nums)
+
+        for k, v in count.items():
+            if v>1:
+                return k
